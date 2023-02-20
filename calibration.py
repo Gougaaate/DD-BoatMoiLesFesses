8aafa0783e420b87d08697447c91d2778a531498
Alex Batard <51515265+alexbatard@users.noreply.github.com>
2023-02-20 15:48:13 +0100

    edited calibration

--- calibration.py
+++ calibration.py
@@ -14,21 +14,16 @@ inclination = 64
 
 def calibrate_mag():
     input("Press enter for north calibration")
-    xn = imu.read_mag_raw()
+    xn = np.array(imu.read_mag_raw())
 
     input("Press enter for south calibration")
-    xs = imu.read_mag_raw()
+    xs = np.array(imu.read_mag_raw())
 
     input("Press enter for west calibration")
-    xw = imu.read_mag_raw()
+    xw = np.array(imu.read_mag_raw())
 
     input("Press enter for up calibration")
-    xu = imu.read_mag_raw()
-
-    xn = np.array(xn)
-    xs = np.array(xs)
-    xw = np.array(xw)
-    xu = np.array(xu)
+    xu = np.array(imu.read_mag_raw())
 
     b = -0.5 * (xn + xs)
     X = np.vstack((xn + b, xw + b, xu + b))
@@ -49,7 +44,7 @@ def calibrate_mag():
     return A, b
 
 
-def angle_degre(u, v):
+def angle_deg(u, v):
     angle_radians = np.arccos(
         (np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))))
     return angle_radians
@@ -58,18 +53,18 @@ def angle_degre(u, v):
 A, b = calibrate_mag()
 
 while True:
-    x1 = imu.read_mag_raw()
+    x1 = np.array(imu.read_mag_raw())
     y1 = np.linalg.inv(A) @ (x1 + b)
     y1 = y1 / np.linalg.norm(y1)
-    a1 = imu.read_accel_raw()
+    a1 = np.array(imu.read_accel_raw())
     a1 = a1 / np.linalg.norm(a1)
     j = np.array([0, 1, 0]).T
     i = np.array([1, 0, 0]).T
     k = np.array([0, 0, 1]).T
-    phichap = np.arcsin(angle_degre(a1.T, j))
-    thetachap = -np.arcsin(angle_degre(a1.T, i))
+    phi_hat = np.arcsin(angle_deg(a1.T, j))
+    theta_hat = -np.arcsin(angle_deg(a1.T, i))
     Rh = rotuv(a1, k)
-    yh = Rh @ y
+    yh = Rh @ y1
     yh1, yh2, yh3 = yh.flatten()
-    psichap = -np.arctan2(yh2, yh1)
-    print(phichap, thetachap, psichap)
+    psi_hat = -np.arctan2(yh2, yh1)
+    print("Phi_hat :", phi_hat, "Theta_hat :", theta_hat, "Psi_hat :", psi_hat)
