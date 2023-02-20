15203cfad4c2725b6fdbcc6e9e89e09bd78111d4
Alex Batard <51515265+alexbatard@users.noreply.github.com>
2023-02-20 15:17:27 +0100

    edited calibration

--- calibration.py
+++ calibration.py
@@ -7,42 +7,48 @@ from drivers.imu9_driver_v2 import *
 
 imu = Imu9IO()
 beta = 46 * 10**(-6)
-I = 64
+inclination = 64
 
 
-def generer_b_mat_a():
-    print("mettre le bateau vers le nord")
-    time.sleep(5.0)
+def calibrate_mag():
+    input("Press enter for north calibration")
     xn = imu.read_mag_raw()
 
-    print("mettre le bateau vers le sud")
-    time.sleep(5.0)
+    input("Press enter for south calibration")
     xs = imu.read_mag_raw()
 
-    print("mettre le bateau vers l'ouest")
-    time.sleep(5.0)
+    input("Press enter for west calibration")
     xw = imu.read_mag_raw()
 
-    print("mettre le bateau vers l'up")
-    time.sleep(5.0)
+    input("Press enter for up calibration")
     xu = imu.read_mag_raw()
-    b = -0.5*(xn+xs)
-    X = np.vstack((xn+b,xw+b,xu+b))
-    yn = np.array([beta*np.cos(64*np.pi/180),0,beta*np.sin(64*np.pi/180)]).T
-    yw = np.array([0, -beta * np.cos(64 * np.pi / 180),-beta*np.sin(64*np.pi/180)]).T
-    yup = np.array([-beta*np.sin(64*np.pi/180),0,beta*np.cos(64*np.pi/180)]).T
-    Y = np.vstack((yn,yw,yup))
-    A=X@np.linalg.inv(Y)
-    return A,b
 
-A,b=generer_b_mat_a()
+    xn = np.array(xn)
+    xs = np.array(xs)
+    xw = np.array(xw)
+    xu = np.array(xu)
+    
+    b = -0.5 * (xn + xs)
+    X = np.vstack((xn + b, xw + b, xu + b))
+    yn = np.array(
+        [beta * np.cos(64 * np.pi / 180), 0,
+         beta * np.sin(64 * np.pi / 180)]).T
+    yw = np.array([
+        0, -beta * np.cos(64 * np.pi / 180), -beta * np.sin(64 * np.pi / 180)
+    ]).T
+    yup = np.array(
+        [-beta * np.sin(64 * np.pi / 180), 0,
+         beta * np.cos(64 * np.pi / 180)]).T
+    Y = np.vstack((yn, yw, yup))
+    A = X @ np.linalg.inv(Y)
+    return A, b
+
+
+A, b = calibrate_mag()
 
 while True:
-    x=imu.read_mag_raw()
-    y=np.linalg.inv(A)@(x+b)
-    y=y/np.linalg.norm(y)
-    a1=imu.read_accel_raw()
-    a1=a1/np.linalg.norm(a1)
-
-
-
+    x = imu.read_mag_raw()
+    y = np.linalg.inv(A) @ (x + b)
+    y = y / np.linalg.norm(y)
+    a1 = imu.read_accel_raw()
+    a1 = a1 / np.linalg.norm(a1)
