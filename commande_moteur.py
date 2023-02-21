7517e721be0af3290a0103a5ed3954f276beb68f
adam <adam.goux--gateau@ensta-bretagne.org>
2023-02-21 11:39:26 +0100

    commande moteur

--- commande_moteur.py
+++ commande_moteur.py
@@ -1,11 +1,15 @@
 from drivers.encoders_driver_v2 import EncoderIO
 from drivers.arduino_driver_v2 import ArduinoIO
 from drivers.imu9_driver_v2 import Imu9IO
+import sys
+sys.path.append("./drivers")
+
 imu = Imu9IO()
 arduino = ArduinoIO()
 encoder = EncoderIO()
 
 arduino.send_arduino_cmd_motor(80,80)
 while True:
-    arduino.get_arduino_cmd_motor()
+    print(arduino.get_arduino_cmd_motor())
+
 
