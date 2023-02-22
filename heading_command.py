81b91c416aeb16e9f0ee8446ae0d70983133530d
Alex Batard <51515265+alexbatard@users.noreply.github.com>
2023-02-22 16:33:24 +0100

    working heading command

--- heading_command.py
+++ heading_command.py
@@ -1,6 +1,6 @@
 import time
 import numpy as np
-from get_current_heading import getHeadingSimple, degToRad
+from get_current_heading import getHeadingSimple
 from get_motors_RPM import getRPM
 
 
@@ -10,73 +10,59 @@ def sawtooth(x):
 
 def chooseRPM(wanted_rpm, goal_rpm_diff):
     goal_rpmL, goal_rpmR = wanted_rpm, wanted_rpm + abs(goal_rpm_diff)
-    if goal_rpm_diff < 0:
+    if goal_rpm_diff > 0:
         print("turn right")
         return goal_rpmR, goal_rpmL
-    elif goal_rpm_diff >= 0:
+    elif goal_rpm_diff <= 0:
         print("turn left")
         return goal_rpmL, goal_rpmR
-
-
-# def adjustPWM(command_pwmL, command_pwmR, command_rpmL, command_rpmR, encoder):
-#     if command_pwmL > 255:
-#         command_pwmL = 255
-#     elif command_pwmL < 0:
-#         command_pwmL = 0
-#     elif command_rpmL - getRPM(encoder)[0] > 50:
-#         command_pwmL += 5
-#     elif command_rpmL - getRPM(encoder)[0] < -50:
-#         command_pwmL -= 5
-
-#     if command_pwmR > 255:
-#         command_pwmR = 255
-#     elif command_pwmR < 0:
-#         command_pwmR = 0
-#     elif command_rpmR - getRPM(encoder)[1] > 50:
-#         command_pwmR = 5
-#     elif command_rpmR - getRPM(encoder)[1] < -50:
-#         command_pwmR = 5
-
-#     return command_pwmL, command_pwmR
 
 
 def followHeading(goal_heading, duration, imu, arduino, encoder, A, b):
     """
     follows the heading for a given duration
-    :param encoder:
-    :param arduino:
-    :param imu:
-    :param goal_heading:
+    :param goal_heading: desired heading
     :param duration: duration of the race
+    :param imu: inertial measurement unit
+    :param arduino: arduino
+    :param encoder: encoder
+    :param A: matrix A
+    :param b: vector b
     :return: none
     """
-    loop_cnt = 0
     file = open("log.txt", "w")
 
     dt = 0.1  # time step
-    K11, K12, K21, K22, K3 = 0.01, 0.01, 0.05, 0.05, 650  # gains
+    K11, K12, K21, K22, K3 = 0.006, 0.04, 0.05, 0.08, 200  # gains
     z1, z2 = 70, 70  # integral terms
-    # command_pwmL, command_pwmR = 80, 80  # pwm values
     global_init_time = time.time()
     while time.time() - global_init_time < duration:
         # init_time = time.time()
         heading = getHeadingSimple(imu, A, b)  # current heading
-        goal_rpm_diff = K3 * sawtooth(
-            degToRad(goal_heading) -
-            degToRad(heading))  # sawtooth takes radians as input
-        wanted_rpm = 3000  # pivot rpm value (constant)
-        goal_rpmL, goal_rpmR = chooseRPM(
-            wanted_rpm, goal_rpm_diff
-        )  # choose the rpm of the motors to turn left or right
-        rpmL, rpmR = getRPM(encoder)  # read the RPM of the motors
-        e1, e2 = goal_rpmL - rpmL, goal_rpmR - rpmR  # error terms
-        z1 += e1 * dt
-        z2 += e2 * dt
-        # command_rpmL = K11 * e1 + K21 * z1
-        # command_rpmR = K12 * e2 + K22 * z2
+        heading_error = goal_heading - heading
+        if heading_error > 70:
+            print("turn right")
+            command_pwmL, command_pwmR = 60, 0
+            rpmL, rpmR = getRPM(encoder)  # read the RPM of the motors
+            goal_rpmL, goal_rpmR = 0, 0
+        elif heading_error < -70:
+            print("turn left")
+            command_pwmL, command_pwmR = 0, 60
+            rpmL, rpmR = getRPM(encoder)  # read the RPM of the motors
+            goal_rpmL, goal_rpmR = 0, 0
+        else:
+            goal_rpm_diff = K3 * heading_error  # desired rpm difference
+            wanted_rpm = 3000  # pivot rpm value (constant)
+            goal_rpmL, goal_rpmR = chooseRPM(
+                wanted_rpm, goal_rpm_diff
+            )  # choose the rpm of the motors to turn left or right
+            rpmL, rpmR = getRPM(encoder)  # read the RPM of the motors
+            e1, e2 = goal_rpmL - rpmL, goal_rpmR - rpmR  # error terms
+            z1 += e1 * dt
+            z2 += e2 * dt
 
-        command_pwmL = K11 * e1 + K21 * z1
-        command_pwmR = K12 * e2 + K22 * z2
+            command_pwmL = K11 * e1 + K21 * z1
+            command_pwmR = K12 * e2 + K22 * z2
 
         if command_pwmL > 255:
             command_pwmL = 255
@@ -87,23 +73,9 @@ def followHeading(goal_heading, duration, imu, arduino, encoder, A, b):
         elif command_pwmR < 0:
             command_pwmR = 0
 
-        # print(e1)
-        # print(e2)
-
-        # command_pwmL, command_pwmR = adjustPWM(command_pwmL, command_pwmR,
-        #                                        command_rpmL, command_rpmR,
-        #                                        encoder)
-
-        # print("###################################")
-        # print("rpmL : ", w1, "rpmR : ", w2)
-        # print("omega bar : ", wbar1, wbar2)
-        # print("z : ", z1, z2)
-        # print("u : ", u1, u2)
-        # print("psi : ", psi)
-
         data_to_write = [
-            rpmL, rpmR, command_pwmL, command_pwmR, heading,
-            abs(goal_heading - heading)
+            rpmL, rpmR, goal_rpmL, goal_rpmR, command_pwmL, command_pwmR,
+            heading, abs(heading_error)
         ]
 
         for data in data_to_write:
@@ -111,13 +83,6 @@ def followHeading(goal_heading, duration, imu, arduino, encoder, A, b):
         file.write("\n")
 
         arduino.send_arduino_cmd_motor(command_pwmL, command_pwmR)
-
-        # end_time = time.time()
-        # if end_time - init_time < dt:
-        #     time.sleep(dt - (end_time - init_time))
-
-        loop_cnt += 1
-        print(loop_cnt)
 
     arduino.send_arduino_cmd_motor(0, 0)  # turn off motors
     file.close()
--- log.py
+++ log.py
@@ -3,34 +3,40 @@ import matplotlib.pyplot as plt
 
 def plot_data():
     with open('log.txt', 'r') as f:
-        speed_left = []
-        speed_right = []
-        input_left = []
-        input_right = []
-        heading_boat = []
+        rpmL = []
+        rpmR = []
+        goal_rpmL = []
+        goal_rpmR = []
+        command_pwmL = []
+        command_pwmR = []
+        heading = []
         error = []
-        for ligne in f.readlines():
-            c1, c2, c3, c4, c5, c6 = ligne.split()
-            speed_left.append(float(c1))
-            speed_right.append(float(c2))
-            input_left.append(float(c3))
-            input_right.append(float(c4))
-            heading_boat.append(float(c5))
-            error.append(float(c6))
-        time = [i for i in range(len(speed_right))]
+        for line in f.readlines():
+            c1, c2, c3, c4, c5, c6, c7, c8 = line.split()
+            rpmL.append(float(c1))
+            rpmR.append(float(c2))
+            goal_rpmL.append(float(c3))
+            goal_rpmR.append(float(c4))
+            command_pwmL.append(float(c5))
+            command_pwmR.append(float(c6))
+            heading.append(float(c7))
+            error.append(float(c8))
+        time = [i for i in range(len(rpmR))]
 
-    plt.figure("vitesse moteur")
-    plt.plot(time, speed_left, label="speed left")
-    plt.plot(time, speed_right, label="speed_right")
+    plt.figure("rpm")
+    plt.plot(time, rpmL, label="rpm left", color="orange")
+    plt.plot(time, rpmR, label="rpm right", color="blue")
+    plt.plot(time, goal_rpmL, label="goal rpm left", color="red")
+    plt.plot(time, goal_rpmR, label="goal rpm right", color="green")
     plt.legend()
     plt.figure("commande moteur")
-    plt.plot(time, input_left, label="input_left")
-    plt.plot(time, input_right, label="input_right")
+    plt.plot(time, command_pwmL, label="command rpm left")
+    plt.plot(time, command_pwmR, label="command rpm right")
     plt.legend()
     plt.figure("heading")
-    plt.plot(time, heading_boat)
+    plt.plot(time, heading)
     plt.legend()
-    plt.figure("erreur")
+    plt.figure("error")
     plt.plot(time, error)
     plt.show()
 
